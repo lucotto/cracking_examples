1d7b09a441fad30a1a75ccd41359cc1a3f71fa5d
lucotto <luca.minchella1@gmail.com>
2022-12-06 15:54:15 +0100

    Completed

--- vector2.c
+++ vector2.c
@@ -1,4 +1,6 @@
 // https://alexandra-zaharia.github.io/posts/implementing-a-vector-in-c/
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct
 {
@@ -70,7 +72,7 @@ int vector_insert(Vector *vector, void *item, int index)
     if (!vector || !vector->data) return -1;
     if (index < 0 || index > vector->count) return -1;
 
-    if (index == vector->count {
+    if (index == vector->count) {
         if (vector_add(vector, item) == -1) return -1;
     } else {
         if (vector->count == vector->capacity) {
@@ -88,3 +90,58 @@ int vector_insert(Vector *vector, void *item, int index)
       return 0;
 }
 
+int vector_delete(Vector *vector, int index)
+{
+    if (!vector || !vector->data) return -1;
+    if (index < 0 || index >= vector->count) return -1;
+
+    vector->count--;
+
+    for (int i = index; i < vector->count; i++)
+        vector->data[i] = vector->data[i+1];
+
+    if (vector->count == vector->capacity / 4 && vector->capacity > VECTOR_INIT_CAPACITY)
+    {
+        if (_vector_resize(vector, vector->capacity / 2) == -1)
+            return -1;
+    }
+}
+
+void vector_int_print(Vector *vector);
+
+int main(void)
+{
+    int i;
+    Vector *vector = vector_create();
+    if (!vector) {
+        fprintf(stderr, "Cannot allocate vector.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int even[3] = {2, 4, 6};
+    for (i = 0; i < 3; i++)
+        vector_add(vector, &even[i]);
+    vector_int_print(vector);
+
+    int odd[3] = {1, 3, 5};
+    vector_insert(vector, &odd[0], 0);
+    vector_insert(vector, &odd[1], 2);
+    vector_insert(vector, &odd[2], 4);
+    vector_int_print(vector);
+
+    for (i = 5; i > 2; i--)
+        vector_delete(vector, i);
+    vector_int_print(vector);
+
+    vector_free(vector);
+
+    return EXIT_SUCCESS;
+}
+
+void vector_int_print(Vector *vector)
+{
+    printf("[ ");
+    for (size_t i = 0; i < vector->count; i++)
+        printf("%d ", *((int * ) vector->data[i]));
+    printf("]\n");
+}
