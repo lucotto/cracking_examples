361423f91ea01fed1c6817780045b809a249474a
lucotto <luca.minchella1@gmail.com>
2022-12-08 22:41:43 +0100

    Added couple methods

--- linked_list.c
+++ linked_list.c
@@ -26,7 +26,7 @@ void printList()
         // set pointer to next node, results in addresses iterations till the end
         ptr = ptr->next;
     }
-    printf(" ]");
+    printf(" ]\n");
 }
 
 // insert link node at first location
@@ -45,13 +45,55 @@ void push_front(int data)
     head = link;
 }
 
-void pop_front()
+void pop_front()    // devo liberare la memoria del nodo che poppo?
 {
-    struct node *toPop = head;
+    // reference to first link
+    struct node *temp = head;
 
+    // mark next of first link (second link) as the new first
     head = head->next;
 
-    return toPop;
+    // return deleted link
+    return temp;
+}
+
+// non funziona, forse servono puntatori doppi?
+void push_back(int data)
+{
+    // declare new pointer with head address
+    struct node *ptr = head;
+    // allocate memory for new node
+    struct node *link = (struct node*)malloc(sizeof(struct node));
+
+    // iterate till last node's pointer
+    while (ptr != NULL)
+    {
+        printf("%d - %d\n", ptr, ptr->data);
+        ptr = ptr->next;
+    }
+
+    // set new node's data to input
+    link->data = data;
+    // set new node's pointer to NULL
+    link->next = ptr;
+    // set old last pointer to new last node
+    ptr = link;
+}
+
+// non funziona DIO PORCO
+void pop_back()
+{
+    struct node *ptr = head;
+
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+
+    ptr = ptr->next;
+
+    printf("Successfully removed last node");
+
 }
 
 int main()
@@ -59,6 +101,11 @@ int main()
     push_front(1);
     push_front(2);
     printList();
-    pop_front();
+    //pop_front();
+    //printList();
+    //push_back(100);
+    push_front(0);
+    printList();
+    pop_back();
     printList();
 }
